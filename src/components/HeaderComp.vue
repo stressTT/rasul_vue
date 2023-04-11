3ba412b90c80475f28708a90225e84229a1386c7
stressTT <batyrkhanb@bk.ru>
2023-04-12 01:14:24 +0600

    asd

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="header">
+    <div class="nav">
+      <div class="topbar">
+        <div class="logo">
+          <img src="@/assets/img/logo_ras.jpg" alt="" />
+          <h3>broome.art</h3>
+        </div>
+        <div class="aside">
+          <p>Upload</p>
+          <p>Contacts</p>
+          <p>Login</p>
+        </div>
+      </div>
+      <div class="article">
+        <p>Лучшие интерьерные картины с высококачественной печатью HD.</p>
+        <input type="text" placeholder="Search for paintings" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="less" scoped>
+.header {
+  background: url(@/assets/img/main_bg2.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-y: -90px;
+  height: 500px;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.4);
+    top: 0;
+    left: 0;
+    z-index: 0;
+  }
+
+  .nav {
+    max-width: 1600px;
+    padding: 0 30px;
+    margin: auto;
+    .topbar {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 80px;
+      position: relative;
+
+      .logo {
+        display: flex;
+        align-items: center;
+        img {
+          height: 50px;
+          width: 50px;
+          border-radius: 50%;
+          margin-right: 8px;
+        }
+        h3 {
+          color: #fff;
+          font-size: 24px;
+        }
+      }
+      .aside {
+        display: flex;
+        align-items: center;
+        p {
+          color: #fff;
+          margin-left: 30px;
+          font-size: 18px;
+          font-weight: 500;
+        }
+      }
+    }
+    .article {
+      display: flex;
+      align-self: center;
+      position: relative;
+      width: 100%;
+      max-width: 630px;
+      margin: auto;
+      padding-top: 100px;
+      flex-wrap: wrap;
+      p {
+        font-size: 34px;
+        color: #fff;
+        line-height: 40px;
+        letter-spacing: -0.2px;
+        font-weight: 500;
+      }
+      input {
+        width: 550px;
+        font-size: 18px;
+        padding: 15px 4px 15px 20px;
+        border-radius: 10px;
+        color: #1d1d1d;
+        margin-top: 30px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/PaintingsComp.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="main">
+    <div class="nav">
+      <p @click="myFilter()" v-bind:class="{ active: isActive }">Popular</p>
+      <p>New</p>
+      <p>Special</p>
+    </div>
+    <h4>Интерьерные картины</h4>
+    <div class="article">
+      <div class="first">
+        <img src="@/assets/img/img1.jpg" alt="" />
+        <img src="@/assets/img/img2.jpg" alt="" />
+        <img src="@/assets/img/img3.jpg" alt="" />
+        <img src="@/assets/img/img4.jpg" alt="" />
+        <img src="@/assets/img/img5.jpg" alt="" />
+        <img src="@/assets/img/img6.jpg" alt="" />
+      </div>
+      <div class="second">
+        <img src="@/assets/img/img6.jpg" alt="" />
+        <img src="@/assets/img/img1.jpg" alt="" />
+        <img src="@/assets/img/img4.jpg" alt="" />
+        <img src="@/assets/img/img3.jpg" alt="" />
+        <img src="@/assets/img/img5.jpg" alt="" />
+        <img src="@/assets/img/img2.jpg" alt="" />
+      </div>
+      <div class="third">
+        <img src="@/assets/img/img5.jpg" alt="" />
+        <img src="@/assets/img/img6.jpg" alt="" />
+        <img src="@/assets/img/img1.jpg" alt="" />
+        <img src="@/assets/img/img3.jpg" alt="" />
+        <img src="@/assets/img/img2.jpg" alt="" />
+        <img src="@/assets/img/img4.jpg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.main {
+  max-width: 1600px;
+  padding: 0 30px;
+  margin: 30px auto;
+
+  .nav {
+    display: flex;
+    margin: auto;
+    justify-content: center;
+    margin-bottom: 30px;
+    p {
+      padding: 15px 20px;
+      font-size: 16px;
+      transition: all 0.3ms;
+      cursor: pointer;
+    }
+    .active {
+      background: #000;
+      color: #fff;
+      margin: 0 10px;
+      border-radius: 24px;
+    }
+  }
+  h4 {
+    font-size: 24px;
+    font-weight: 500;
+    letter-spacing: -0.2px;
+  }
+  .article {
+    margin-top: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: space-between;
+    div {
+      width: 520px;
+
+      img {
+        width: 520px;
+        object-fit: contain;
+        align-self: flex-start;
+        height: fit-content;
+
+        margin-bottom: 16px;
+      }
+    }
+  }
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+  methods: {
+    myFilter: function () {
+      console.log("123");
+      this.isActive = !this.isActive;
+      // some code to filter users
+    },
+  },
+};
+</script>
